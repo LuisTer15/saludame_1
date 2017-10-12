69c7eb97b96afad8b24257ed468bbb93101fcd30
Luis Ternera <lternerah@gmail.com>
2017-10-12 12:23:45 -0500

    Changing for saludame_2 challenge

--- solution.rb
+++ solution.rb
@@ -1,13 +1,13 @@
 require 'sinatra'
 
-get "/" do
+get "/makers/:nombre" do
 	unless params[:nombre]
 		<<-HTML
 			<h1>Hola desconocido!</h1>
 		HTML
 	else
 		<<-HTML
-			<h1>Hola #{params[:nombre]}!</h1>
+			<h1>Hola #{params[:nombre].capitalize}!</h1>
 		HTML
 	end
 end
