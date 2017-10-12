6db36f068ce12a980ca1f5bd5f5d7ac42c9887b7
Luis Ternera <lternerah@gmail.com>
2017-10-12 12:56:01 -0500

    Changing for saludame_3 challenge

--- solution.rb
+++ solution.rb
@@ -1,13 +1,10 @@
 require 'sinatra'
 
-get "/makers/:nombre" do
-	unless params[:nombre]
-		<<-HTML
-			<h1>Hola desconocido!</h1>
-		HTML
-	else
-		<<-HTML
-			<h1>Hola #{params[:nombre].capitalize}!</h1>
-		HTML
-	end
+get '/' do
+	erb :index
+end
+
+post '/usuario/' do
+	@nombre = "#{params[:nombre]}"
+	erb :user
 end
